78e48849998c10a330741b51d601a38bf53ef472
osmiumic <158513254+osmiumic@users.noreply.github.com>
2024-09-17 23:28:55 +0100

    Update te_common.py
    fix prime limit mistake (the prime after 97 is 101 not 113, so may as well stop at 97; if it was 113 then itd be a nice stopping place as that's another record prime gap, but as is, stopping one after a record prime gap makes sense)

--- te_common.py
+++ te_common.py
@@ -7,7 +7,7 @@ from scipy import linalg
 from sympy.matrices import Matrix, normalforms
 from sympy import gcd
 
-PRIME_LIST = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 113]
+PRIME_LIST = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 RATIONAL_WEIGHT_LIST = ["equilateral"]
 ALGEBRAIC_WEIGHT_LIST = RATIONAL_WEIGHT_LIST + ["wilson", "benedetti"]
 
